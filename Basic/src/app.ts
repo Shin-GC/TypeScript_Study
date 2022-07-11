8f391991e2bdef62be9055d4044b0cde842fd922
Shin-GC <tlsrhkdcjs12@naver.com>
2022-07-11 23:51:28 +0900

    Add Type 사용자 정의를 통해 객체 생성

--- Basic/src/app.ts
+++ Basic/src/app.ts
@@ -1,3 +1,5 @@
+import { SpawnSyncOptionsWithBufferEncoding } from "child_process";
+
 type User = {
   name: string;
   age?: number;
@@ -53,4 +55,30 @@ let sum: operation = {
   },
 };
 
-console.log(sum.output(5));
+// console.log(sum.output(5));
+
+type heroGame = {
+  title: string;
+  price: string;
+  desc: string;
+  category: string;
+  platform: string;
+};
+
+let heroGame_A: heroGame = {
+  title: "DC 언체인드",
+  price: "인앱 구매 제공",
+  desc: "DC 히어로 & 빌런 각각의 개성은 물론, 액션의 재미까지!",
+  category: "액션",
+  platform: "모바일",
+};
+
+let heroGame_B: heroGame = {
+  title: "MARVEL 퓨처파이트",
+  price: "",
+  desc: "마블 유니버스 히어로와 함께하는 대규모 블록버스터 액션 RPG!",
+  category: "롤플레잉",
+  platform: "모바일",
+};
+
+console.log("%o\n%o", heroGame_A, heroGame_B);
